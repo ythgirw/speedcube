ff65375c25768ff7513e81b287874f8df231c305
ythgirw <kevin.wrighty@gmail.com>
2023-11-29 22:11:48 +0000

    Remove unrelated Optechal project files

--- templates/button_updater.js
+++
@@ -1,9 +0,0 @@
-function updateFileName(labelId, fileInputId) {
-  var fileInput = document.getElementById(fileInputId);
-  var label = document.getElementById(labelId);
-  if (fileInput.files.length > 0) {
-    label.textContent = fileInput.files[0].name;
-  } else {
-    label.textContent = "No file chosen";
-  }
-}
